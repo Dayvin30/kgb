108c11804548076097e1c54803bdb949d743d7b7
Dayvin30 <dayvin30@gmail.com>
2022-05-25 21:13:17 +0200

    uihuln

--- include/gestionBDD.php
+++ include/gestionBDD.php
@@ -129,7 +129,7 @@ function ModifierCibles($id, $Nom, $Prenom, $Birthday, $Nationalite)
     $connexion=connexionBDD();
 
      
-    $requete='update cibles set nom = :nom, prenom= :prenom,birthday= :birthday,nationalite= :Nationalite where nom_de_code = :id;';
+    $requete='update cibles set nom = :nom, prenom= :prenom,birthday= :birthday,nationalite= :nationalite where nom_de_code = :id;';
     echo($requete);
     
    
