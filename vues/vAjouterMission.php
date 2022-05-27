d7c7d9ab683837e59e7b0798d60eaebfda8e718b
Dayvin30 <dayvin30@gmail.com>
2022-05-27 16:32:12 +0200

    update functions

--- vues/vAjouterMission.php
+++ vues/vAjouterMission.php
@@ -17,7 +17,7 @@
 <div class="bg-image"
  
      style="background-image: url('https://mdbootstrap.com/img/Photos/Others/images/76.jpg');
-            height: 100vh">
+            height: 100">
 <?php include('navbar.php');
 ?>
 
